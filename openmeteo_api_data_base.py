1d1d46c3e7f59342079a7f68fe9eb759bed9d2bb
ghjoss <nlgithub@glenwoodconsulting.com>
2026-06-06 07:10:02 -0700

    Initial Release

---
+++ openmeteo_api_data_base.py
@@ -0,0 +1,9 @@
+LONGITUDE = 0           #Prime Meridian
+LATITUDE = 51.28
+TIMEZONE = "auto"
+#example: Boston MA,USA
+#LONGITUDE = -71.147761      #Boston
+#LATITUDE = 42.3051993
+#TIMEZONE = "America/New_York"
+
+UNITS = "imperial"  # imperial / metric
